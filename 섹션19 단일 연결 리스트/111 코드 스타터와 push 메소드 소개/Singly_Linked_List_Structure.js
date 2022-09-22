891d36bf0d2a94953ab0d40ec5630d30d14c9248
starplanter93 <starplanter93@gmail.com>
2022-09-22 14:16:06 +0900

    단일연결리스트 pop메서드 구현

--- 섹션19 단일 연결 리스트/111 코드 스타터와 push 메소드 소개/Singly_Linked_List_Structure.js
+++ 섹션19 단일 연결 리스트/111 코드 스타터와 push 메소드 소개/Singly_Linked_List_Structure.js
@@ -26,6 +26,33 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    pop(){
+        //리스트에 아무것도 없을 경우 undefined 반환
+        //head가 null이거나 길이가 0이면 리스트가 빈 것
+        //비어있지 않을 경우 테일에 이를 때까지 전체 리스트를 따라가기
+        //테일 바로 전 노드 찾기 -> 즉 테일에 이를때까지 따라가면서 이전 노드가 어떤 것이였는지 계속 추적해야함
+        //마지막에서 두번째 노드의 next를 null로 설정, tail값을 마지막에서 두번째 노드로 업데이트
+        //길이를 하나 감소시키고, 방금 제거한 노드를 반환-> 반환하기 위해서는 제거된 노드를 별도의 변수에 저장해놔야함
+        if(!this.head){
+            return undefined;
+        } else {
+            let current = this.head;
+            let newTail = current;
+            while(current.next){
+                newTail = current;
+                current = current.next;
+            }
+            this.tail = newTail;
+            this.tail.next = null;
+            this.length--;
+            if(this.length === 0){
+                this.head === null;
+                this.tail === null;
+            }
+            return current;
+        }
+
+    }
 }
 
 // var first = new Node("Hi")
@@ -37,7 +64,9 @@ class SinglyLinkedList{
 var list = new SinglyLinkedList()
 list.push("HELLO")
 list.push("GOODBYE")
-
+console.log(list);
+console.log(list.pop());
+console.log(list.pop());
 console.log(list);
 
 
