fe8b4bfbd7a72af7a3fd5454687783bc1ca5dcc9
starplanter93 <starplanter93@gmail.com>
2023-08-07 17:43:12 +0900

    단일연결리스트 get

---
+++ section19 단일연결리스트/5. get.js
@@ -0,0 +1,88 @@
+/**
+ * 1. index를 인자로 받는다.
+ * 2. 인덱스가 음수이거나, 리스트의 길이보다 같거나 큰경우 동작 불가. null을 반환한다.
+ * 3. 루프를 통해 인덱스가 지정한 위치에 이를 때까지 반복해서 이동한 다음, 해당 인덱스 위치에있는 노드를 반환.
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let pre = current;
+    while (current.next) {
+      pre = current;
+      current = current.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+    while (index !== count) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push('hello');
+list.push('goodbye');
