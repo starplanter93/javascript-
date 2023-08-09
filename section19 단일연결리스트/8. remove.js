43aab234ed86cc589fa1fab4dfdec2d80d33c315
starplanter93 <starplanter93@gmail.com>
2023-08-09 17:42:18 +0900

    단일연결리스트 remove

---
+++ section19 단일연결리스트/8. remove.js
@@ -0,0 +1,122 @@
+/**
+ * 1. index를 인자로 받는다.
+ * 2. index가 음수이거나 리스트의 길이보다 같거나 크면 undefined 리턴
+ * 3. index가 리스트의 길이-1 과 같다면 pop
+ * 4. index가 0이면 shift
+ * 5. 위의 경우가 아니면, get에 index-1을 넣어 직전 노드를 찾는다.
+ * 6. 이전노드의 next를 대상 노드의 next노드로 설정한다.
+ * 7. 길이를 1 줄이고, 제거된 노드를 리턴한다.
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let pre = current;
+    while (current.next) {
+      pre = current;
+      current = current.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+    while (index !== count) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push('hello');
+list.push('goodbye');
