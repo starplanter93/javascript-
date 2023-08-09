4a6f66a24c7a015384223c78b706cca16c6fc0ed
starplanter93 <starplanter93@gmail.com>
2023-08-09 18:32:53 +0900

    단일연결리스트 reverse

---
+++ section19 단일연결리스트/9. reverse.js
@@ -0,0 +1,139 @@
+/**
+ * 1. 인자는 받지않음. head와 tail을 바꾼다.
+ * 2. next라는 변수를 만든다.
+ * 3. prev라는 변수를 만든다.
+ * 4. node라는 변수를 만들고, 현재의 head로 초기화시킨다.
+ * 5. 리스트에서 루프를 돈다.
+ * 6. next 변수에 현재 노드의 next를 저장한다.
+ * 7. 현재 노드의 next를 앞에 있던 노드로 설정한다.
+ * 8. 현재의 노드를 prev변수에 저장한다.
+ * 9. node변수에 next변수 값을 할당한다.
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let pre = current;
+    while (current.next) {
+      pre = current;
+      current = current.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+    while (index !== count) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push('hello');
+list.push('goodbye');
