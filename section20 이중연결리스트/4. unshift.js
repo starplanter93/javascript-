d19d7604ade3f585257127f719342d5e71c766d1
starplanter93 <starplanter93@gmail.com>
2023-08-28 17:38:24 +0900

    이중연결리스트 unshift

---
+++ section20 이중연결리스트/4. unshift.js
@@ -0,0 +1,81 @@
+/**
+ * 1. 값을 받아 새 노드를 만든다.
+ * 2. 리스트의 길이가 0이라면 새노드를 리스트의 헤드와 테일로 설정한다.
+ * 3. 그렇지 않다면 현재 헤드의 prev 속성을 새 노드로 설정한다.
+ * 4. 새 노드의 next 속성을 현재 헤드로 설정한다.
+ * 5. 새 노드를 헤드로 설정한다.
+ * 6. 길이를 1 증가시키고 리스트를 리턴한다.
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
