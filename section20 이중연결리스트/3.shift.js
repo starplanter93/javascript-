95ddeaec5095f62c83399b83cc4f28f86098632f
starplanter93 <starplanter93@gmail.com>
2023-08-28 17:31:51 +0900

    이중연결리스트 shift

---
+++ section20 이중연결리스트/3.shift.js
@@ -0,0 +1,66 @@
+/**
+ * 1. 리스트 길이가 0이면 undefined 출력
+ * 2. 현재 head를 변수에 저장
+ * 3. 길이가 1이라면 head와 tail을 null로 설정
+ * 4. head를 이전 head의 다음 노드로 업데이트
+ * 5. 길이를 줄이고 이전 head를 리턴
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+}
